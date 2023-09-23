02e9b0ace6ffa06840abdc2232f23fe3b44b99f7
User <User@DESKTOP-AMGP8UK>
2023-09-23 10:56:29 +0500

    Добавьте файлы проекта.

---
+++ PZ_05 ShirokovDaniil2pk2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int n = FindFibonacciNumber(a);
+
+        Console.WriteLine(n);
+    }
+
+    static int FindFibonacciNumber(int a)
+    {
+        int prev = 0;
+        int curr = 1;
+        int n = 1;
+
+        while (curr < a)
+        {
+            int temp = curr;
+            curr = prev + curr;
+            prev = temp;
+            n++;
+        }
+
+        if (curr == a)
+        {
+            return n;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
